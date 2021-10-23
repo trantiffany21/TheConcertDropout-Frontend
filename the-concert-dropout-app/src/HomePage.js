cb6a69b4e317d69a26e56dc1f242484b764babd6
knowid <davis.ihsaan@gmail.com>
2021-10-23 16:34:29 -0400

    Added HTML LOGIN SIGNUP pages

--- the-concert-dropout-app/src/HomePage.js
+++ the-concert-dropout-app/src/HomePage.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
-
+import './style.css'
 class HomePage extends Component {
     render() {
         return (
             <div>
-                <p>Home Page</p>
+                {/* <!-- Section: top-hero-text --> */}
+    <section id="top-hero-text">
+        <div class="top-hero-text-container">
+            <div class="texts">
+                <h1 class="main-heading">Get peaceful state of mind from the music</h1>
+                <p class="primary-text">With over 10's of original evergreen songs on the go...</p>
+                <p class="secondary-text">Search your favorite music with the best ever music-based search engine.</p>
+            </div>
+            <div class="buttons">
+                <a href="login" class="btn-primary">Login</a>
+                <a href="signup" class="btn-secondary">Register</a>
+            </div>
+        </div>
+    </section>
+
             </div>
         )
     }
