d17a9b876bd6fa53c93e477f90eaf4fbdc3a969e
criscama <cristian_camacho@aol.com>
2021-10-26 00:31:09 -0400

    Cleaned up some html. Added fetch for loggin in and signing up. That is all it does nothing else.

--- the-concert-dropout-app/src/SignupPage.js
+++ the-concert-dropout-app/src/SignupPage.js
@@ -1,36 +1,58 @@
 import React, { Component } from 'react'
 
 class SignupPage extends Component {
+
+    onSubmit = (event) => {
+        event.preventDefault()
+        fetch('http://localhost:3003/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password,
+                email: this.state.email
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        }).then(res => console.log(res.json()))
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    } 
+
     render() {
         return (
             <div id="register">
-            <div class="container register-container">
-                <div class="main-register-area">
-                    <div class="image-view"></div>
-                    <div class="register-view">
-                        <div class="texts">
-                            <a href="index.html" class="back-text"><i class="fa fa-arrow-left"></i>&nbsp; back to home</a>
+            <div className="container register-container">
+                <div className="main-register-area">
+                    <div className="image-view"></div>
+                    <div className="register-view">
+                        <div className="texts">
+                            <a href="/" className="back-text"><i className="fa fa-arrow-left"></i>&nbsp; back to home</a>
                             <h1>Register</h1>
                             <p>Create an account to browse into the largest evergreen songs library !</p>
                         </div>
-                        <form action="">
-                            <div class="input-group">
-                                <label for="username">Username</label>
-                                <input type="text" name="username" id="username" placeholder="Enter username"></input>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="input-group">
+                                <label htmlFor="username">Username</label>
+                                <input type="text" name="username" id="username" placeholder="Enter username" onChange={this.handleChange} />
                             </div>
-                            <div class="input-group">
-                                <label for="email">Email</label>
-                                <input type="email" name="email" id="email" placeholder="Enter email"></input>
+                            <div className="input-group">
+                                <label htmlFor="email">Email</label>
+                                <input type="email" name="email" id="email" placeholder="Enter email" onChange={this.handleChange} />
                             </div>
-                            <div class="input-group">
-                                <label for="password">Password</label>
-                                <input type="password" name="password" id="password" placeholder="Enter pass"></input>
+                            <div className="input-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" name="password" id="password" placeholder="Enter pass" onChange={this.handleChange} />
                             </div>
-                            <div class="input-group">
-                                <input type="submit" value="Register"></input>
+                            <div className="input-group">
+                                <input type="submit" value="Register" />
                             </div>
-                            <div class="input-group">
-                                <p>Already have an account ? Wanna<a href="login.html" class="login-text">&nbsp;Login ?</a></p>
+                            <div className="input-group">
+                                <p>Already have an account ? Wanna<a href="login" className="login-text">&nbsp;Login ?</a></p>
                             </div>
                         </form>
                     </div>
